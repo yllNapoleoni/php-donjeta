e5c935ecf7e2c7899bdae1bbfc29aea42036e7d5
yllNapoleoni <166026966+yllNapoleoni@users.noreply.github.com>
2025-03-04 20:43:14 +0100

    day 1

---
+++ day1/main.php
@@ -0,0 +1,30 @@
+<?php 
+echo "hello world";
+
+$school = "digital school";
+
+echo "<br>";
+
+$numri1 = 20;
+$numri2 = 1298;
+
+echo $numri1 + $numri2;
+echo "<br>";
+echo $numri1 * $numri2;
+echo "<br>";
+echo $numri1 / $numri2;
+
+echo "<br>";
+
+
+
+$numri3 = 10;
+$numri4 = 2;
+
+
+echo $numri3 % $numri4;
+echo "<br>";
+
+
+echo "i love $school";
+?>
