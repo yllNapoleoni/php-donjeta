124fe73c305c5bf11ffa6d5cc6aad02f29483cff
yllNapoleoni <166026966+yllNapoleoni@users.noreply.github.com>
2025-06-03 20:42:26 +0200

    day3

--- PersonalProject/index.php
+++ PersonalProject/index.php
@@ -23,11 +23,12 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.html">Home</a></li>
-                            <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-                            <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-                            <li class="nav-item"><a class="nav-link" href="course.html">Courses</a></li>
-                            <li class="nav-item"><a class="nav-link" href="pricing.html">Pricing</a></li>
-                            <li class="nav-item"><a class="nav-link" href="faq.html">FAQ</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                            <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                            <li class="nav-item"><a class="nav-link" href="course.php">Courses</a></li>
+                            <li class="nav-item"><a class="nav-link" href="pricing.php">Pricing</a></li> 
+                            <li class="nav-item"><a class="nav-link" href="users.php">Users</a></li> 
+                            <li class="nav-item"><a class="nav-link" href="faq.php">FAQ</a></li> 
                             <li class="nav-item d-flex align-items-center ms-3">
                                 <button id="darkModeToggle" class="btn btn-outline-light btn-sm">🌙 Dark Mode</button>
                                 </li>
